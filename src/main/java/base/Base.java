866c8ef27ce146bb6ea7c1089b8b169a4797c2fb
Vladyslav <holivervlad@gmail.com>
2019-11-04 17:56:58 +0200

    test first commit

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -4,7 +4,7 @@ import configurationManager.BaseConfiguration;
 import pages.OpenPlayGround;
 
 public class Base {
-    private static String originUrl = "";
+    private static String originUrl = "https://curious-moose-50xavl-dev-ed.lightning.force.com/";
 
     public OpenPlayGround openPlayGroundPage() {
         BaseConfiguration.getDriver().get(originUrl);
